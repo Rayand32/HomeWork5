b72d950b029df7140ab12e48a6c7d10ff59f7b93
Rayand <nopechan2020@gmail.com>
2022-04-15 17:54:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int firstNumber = 100;
+        int secondNumber = 50;
+        int numbers = 2;
+        int result;
+
+        System.out.println("Программа позволяет вычеслить среднее арифметическое из двух чисел");
+
+        result = (firstNumber + secondNumber) / numbers;
+
+        System.out.print("Среднее арифметическое число: " + result);
+
+    }
+
+
+}
